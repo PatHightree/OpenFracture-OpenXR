76ede922e142da032350f7eb96bcabc27906e2fc
Patrick Hogenboom <patrick.hogenboom@pm.me>
2022-01-16 01:09:09 +0100

    Gun improvements + locomotion
    Both hands can pick up and fire the gun
    Sanity checks before firing gun
    Tuned character dimensions
    Added continuous locomotion to left hand
    Added snap turn to right hand

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GunControl : MonoBehaviour
 {
     [Header("Input")]
-    public InputActionReference ShootActionReference;
+    public InputActionReference ShootLeftHandActionReference;
+    public InputActionReference ShootRightHandActionReference;
     
     [Space] 
     [Header("Shooting")] 
@@ -12,26 +14,38 @@ public class GunControl : MonoBehaviour
     public GameObject Projectile;
     public float InitialVelocity;
 
+    private XRGrabInteractable m_grabInteractable;
+    private IXRSelectInteractor m_interactorHoldingTheGun;
+
     private void OnEnable()
     {
-        ShootActionReference.action.performed += OnShootPerformed;
+        ShootLeftHandActionReference.action.performed += OnShootPerformed;
+        ShootRightHandActionReference.action.performed += OnShootPerformed;
+        
+        m_grabInteractable = GetComponent<XRGrabInteractable>();
+        m_grabInteractable.selectEntered.AddListener(selectEnterEventArgs => { m_interactorHoldingTheGun = selectEnterEventArgs.interactorObject;});
+        m_grabInteractable.selectExited.AddListener(_ => { m_interactorHoldingTheGun = null;});
     }
 
     private void OnDisable()
     {
-        ShootActionReference.action.performed -= OnShootPerformed;
+        ShootLeftHandActionReference.action.performed -= OnShootPerformed;
+        ShootRightHandActionReference.action.performed -= OnShootPerformed;
     }
 
     private void OnShootPerformed(InputAction.CallbackContext _context)
     {
+        // Don't shoot the gun if it isn't grabbed
+        if (!m_grabInteractable.interactorsSelecting.Contains(m_interactorHoldingTheGun)) return;
+        // Don't shoot if the trigger that was pulled isn't on the controller that's holding the gun
+        if (_context.action.ToString().ToLower().Contains("left") != m_interactorHoldingTheGun.ToString().ToLower().Contains("left") &&
+            _context.action.ToString().ToLower().Contains("right") != m_interactorHoldingTheGun.ToString().ToLower().Contains("right")) return;
+        
         // Remove other projectiles from the scene
         foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Projectile"))
             Destroy(obj);
 
         GameObject projectileInstance = Instantiate(Projectile, LaunchTransform.position, Quaternion.identity);
         projectileInstance.GetComponent<Rigidbody>().velocity = InitialVelocity * LaunchTransform.forward;
-
-        // GameObject projectileInstance = Instantiate(Projectile, m_controllerPosition.ReadValue<Vector3>(), Quaternion.identity);
-        // projectileInstance.GetComponent<Rigidbody>().velocity = InitialVelocity * (m_controllerRotation.ReadValue<Quaternion>() * Vector3.forward);
     }
 }
