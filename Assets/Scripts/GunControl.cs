ea5213fdcebd5be7f6830489e2ab40207f9ecc78
Patrick Hogenboom <patrick.hogenboom@pm.me>
2022-01-20 00:11:55 +0100

    Added namespace

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -3,70 +3,70 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class GunControl : MonoBehaviour
+namespace OpenXR_OpenFracture
 {
-    [Header("Input")]
-    public InputActionReference ShootLeftHandActionReference;
-    public InputActionReference ShootRightHandActionReference;
-    
-    [Space] 
-    [Header("Shooting")] 
-    public Transform LaunchTransform;
-    public GameObject Projectile;
-    public float InitialVelocity;
-    public AnimationCurve HapticEffect;
-
-    private XRGrabInteractable m_grabInteractable;
-    private IXRSelectInteractor m_interactorHoldingTheGun;
-
-    private void OnEnable()
+    public class GunControl : MonoBehaviour
     {
-        ShootLeftHandActionReference.action.performed += OnShootPerformed;
-        ShootRightHandActionReference.action.performed += OnShootPerformed;
-        
-        m_grabInteractable = GetComponent<XRGrabInteractable>();
-        m_grabInteractable.selectEntered.AddListener(selectEnterEventArgs => { m_interactorHoldingTheGun = selectEnterEventArgs.interactorObject;});
-        m_grabInteractable.selectExited.AddListener(_ => { m_interactorHoldingTheGun = null;});
-    }
+        [Header("Input")] public InputActionReference ShootLeftHandActionReference;
+        public InputActionReference ShootRightHandActionReference;
 
-    private void OnDisable()
-    {
-        ShootLeftHandActionReference.action.performed -= OnShootPerformed;
-        ShootRightHandActionReference.action.performed -= OnShootPerformed;
-    }
+        [Space] [Header("Shooting")] public Transform LaunchTransform;
+        public GameObject Projectile;
+        public float InitialVelocity;
+        public AnimationCurve HapticEffect;
 
-    private void OnShootPerformed(InputAction.CallbackContext _context)
-    {
-        // Don't shoot the gun if it isn't grabbed
-        if (!m_grabInteractable.interactorsSelecting.Contains(m_interactorHoldingTheGun)) return;
-        // Don't shoot if the trigger that was pulled isn't on the controller that's holding the gun
-        if (_context.action.ToString().ToLower().Contains("left") != m_interactorHoldingTheGun.ToString().ToLower().Contains("left") &&
-            _context.action.ToString().ToLower().Contains("right") != m_interactorHoldingTheGun.ToString().ToLower().Contains("right")) return;
+        private XRGrabInteractable m_grabInteractable;
+        private IXRSelectInteractor m_interactorHoldingTheGun;
 
-        // Vibrate controller
-        XRBaseControllerInteractor controller = (XRBaseControllerInteractor)m_grabInteractable.firstInteractorSelecting;
-        if (controller != null)
+        private void OnEnable()
         {
-            // controller.SendHapticImpulse(1f, 0.05f);
-            StartCoroutine(PlayHapticEffect(controller, HapticEffect));
+            ShootLeftHandActionReference.action.performed += OnShootPerformed;
+            ShootRightHandActionReference.action.performed += OnShootPerformed;
+
+            m_grabInteractable = GetComponent<XRGrabInteractable>();
+            m_grabInteractable.selectEntered.AddListener(selectEnterEventArgs => { m_interactorHoldingTheGun = selectEnterEventArgs.interactorObject; });
+            m_grabInteractable.selectExited.AddListener(_ => { m_interactorHoldingTheGun = null; });
         }
-        
-        // Remove other projectiles from the scene
-        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Projectile"))
-            Destroy(obj);
 
-        GameObject projectileInstance = Instantiate(Projectile, LaunchTransform.position, Quaternion.identity);
-        projectileInstance.GetComponent<Rigidbody>().velocity = InitialVelocity * LaunchTransform.forward;
-    }
-
-    private IEnumerator PlayHapticEffect(XRBaseControllerInteractor _controller, AnimationCurve _hapticEffect)
-    {
-        float triggerTime = Time.time;
-        while (Time.time < triggerTime + _hapticEffect[_hapticEffect.length-1].time)
+        private void OnDisable()
         {
-            float amplitude = _hapticEffect.Evaluate(Time.time - triggerTime);
-            _controller.SendHapticImpulse(amplitude, Time.deltaTime);
-            yield return null;
+            ShootLeftHandActionReference.action.performed -= OnShootPerformed;
+            ShootRightHandActionReference.action.performed -= OnShootPerformed;
+        }
+
+        private void OnShootPerformed(InputAction.CallbackContext _context)
+        {
+            // Don't shoot the gun if it isn't grabbed
+            if (!m_grabInteractable.interactorsSelecting.Contains(m_interactorHoldingTheGun)) return;
+            // Don't shoot if the trigger that was pulled isn't on the controller that's holding the gun
+            if (_context.action.ToString().ToLower().Contains("left") != m_interactorHoldingTheGun.ToString().ToLower().Contains("left") &&
+                _context.action.ToString().ToLower().Contains("right") != m_interactorHoldingTheGun.ToString().ToLower().Contains("right")) return;
+
+            // Vibrate controller
+            XRBaseControllerInteractor controller = (XRBaseControllerInteractor)m_grabInteractable.firstInteractorSelecting;
+            if (controller != null)
+            {
+                // controller.SendHapticImpulse(1f, 0.05f);
+                StartCoroutine(PlayHapticEffect(controller, HapticEffect));
+            }
+
+            // Remove other projectiles from the scene
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Projectile"))
+                Destroy(obj);
+
+            GameObject projectileInstance = Instantiate(Projectile, LaunchTransform.position, Quaternion.identity);
+            projectileInstance.GetComponent<Rigidbody>().velocity = InitialVelocity * LaunchTransform.forward;
+        }
+
+        private IEnumerator PlayHapticEffect(XRBaseControllerInteractor _controller, AnimationCurve _hapticEffect)
+        {
+            float triggerTime = Time.time;
+            while (Time.time < triggerTime + _hapticEffect[_hapticEffect.length - 1].time)
+            {
+                float amplitude = _hapticEffect.Evaluate(Time.time - triggerTime);
+                _controller.SendHapticImpulse(amplitude, Time.deltaTime);
+                yield return null;
+            }
         }
     }
 }
--- Assets/Scripts/RayToggler.cs
+++ Assets/Scripts/RayToggler.cs
@@ -2,39 +2,42 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class RayToggler : MonoBehaviour
+namespace OpenXR_OpenFracture
 {
-    public InputActionReference ActivateReference;
-
-    private XRRayInteractor m_rayInteractor;
-    private bool m_enabled;
-
-    private void Awake()
+    public class RayToggler : MonoBehaviour
     {
-        m_rayInteractor = GetComponent<XRRayInteractor>();
-    }
+        public InputActionReference ActivateReference;
 
-    private void OnEnable()
-    {
-        ActivateReference.action.performed += ToggleRay;
-        ActivateReference.action.canceled += ToggleRay;
-    }
+        private XRRayInteractor m_rayInteractor;
+        private bool m_enabled;
 
-    private void OnDisable()
-    {
-        ActivateReference.action.performed -= ToggleRay;
-        ActivateReference.action.canceled -= ToggleRay;
-    }
+        private void Awake()
+        {
+            m_rayInteractor = GetComponent<XRRayInteractor>();
+        }
 
-    private void ToggleRay(InputAction.CallbackContext _context)
-    {
-        // m_enabled = _context.control.IsActuated();
-        m_enabled = _context.control.IsPressed();
-    }
+        private void OnEnable()
+        {
+            ActivateReference.action.performed += ToggleRay;
+            ActivateReference.action.canceled += ToggleRay;
+        }
 
-    private void LateUpdate()
-    {
-        if (m_rayInteractor.enabled != m_enabled)
-            m_rayInteractor.enabled = m_enabled;
+        private void OnDisable()
+        {
+            ActivateReference.action.performed -= ToggleRay;
+            ActivateReference.action.canceled -= ToggleRay;
+        }
+
+        private void ToggleRay(InputAction.CallbackContext _context)
+        {
+            // m_enabled = _context.control.IsActuated();
+            m_enabled = _context.control.IsPressed();
+        }
+
+        private void LateUpdate()
+        {
+            if (m_rayInteractor.enabled != m_enabled)
+                m_rayInteractor.enabled = m_enabled;
+        }
     }
 }
